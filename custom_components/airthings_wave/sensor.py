9a9e9590df1a373bab1d7432ea46f3c3db996317
Lyman Epp <lyman@epptech.com>
2022-06-10 11:59:02 -0400

    Make imperial temperatures compatible with HA 2022.4 changes

--- custom_components/airthings_wave/sensor.py
+++ custom_components/airthings_wave/sensor.py
@@ -265,7 +265,7 @@ class AirthingsSensor(SensorEntity):
         return self._name
 
     @property
-    def state(self):
+    def native_value(self):
         """Return the state of the device."""
         return self._state
 
@@ -280,7 +280,7 @@ class AirthingsSensor(SensorEntity):
         return self._sensor_specifics.device_class
 
     @property
-    def unit_of_measurement(self):
+    def native_unit_of_measurement(self):
         """Return the unit the value is expressed in."""
         return self._sensor_specifics.unit
 
